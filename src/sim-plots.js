8187100dbedd19da27e5d4eb4955b77ebb0977bd
Matt Bomhoff <mbomhoff@email.arizona.edu>
2017-12-19 14:40:57 -0700

    Remove old test code

--- src/sim-plots.js
+++ src/sim-plots.js
@@ -1,5 +1,3 @@
-import * as d3 from 'd3';
-import {simMatrixToObj} from './utils.js'
 import {heatmap} from './heatmap.js'
 import {edgeboundary} from './edgeboundary.js'
 import {pcoaPlot} from './pcoaPlot.js'
