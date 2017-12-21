6ec0cf17e9f039adc6c4e0ff958d91ea7361a530
Matt Bomhoff <mbomhoff@email.arizona.edu>
2017-12-21 13:26:43 -0700

    Add Centrifuge bubble plot

---
+++ src/bubblePlot.js
@@ -0,0 +1,46 @@
+import * as d3 from 'd3';
+import * as bubble from 'd3.chart.bubble-matrix';
+
+function bubblePlot(id, data, params) { // TODO split data processing and rendering into separate functions
+    params = params || {};
+
+    // Convert input data into format required by d3.chart.bubble-matrix
+    var columns = {};
+    var rows = {};
+    var maxValue = 10;
+    data.forEach(function(row) {
+        columns[row.sample] = 1;
+
+        if (typeof rows[row.Name] === 'undefined')
+            rows[row.Name] = {};
+        rows[row.Name][row.sample] = row.Abundance;
+
+        maxValue = Math.max(maxValue, row.Abundance);
+    });
+
+    var rows2 = [];
+    Object.keys(rows).sort().reverse().forEach(function(name) {
+        var values =
+            Object.keys(columns).map(sample => {
+                return [ (rows[name][sample] || 0) / maxValue, 0.7 ];
+            });
+        rows2.push({
+            name: name,
+            values: values
+        });
+    });
+
+    var data2 = { columns: Object.keys(columns), rows: rows2 };
+
+    var width = Object.keys(columns).length * 200;
+    var height = Object.keys(rows).length * 40;
+
+    var chart = d3.select('#plot').append('svg')
+                  .chart('BubbleMatrix')
+                  .width(width).height(height)
+                  .slanted(true);
+
+    chart.draw(data2);
+}
+
+export { bubblePlot };
--- src/sim-plots.js
+++ src/sim-plots.js
@@ -1,7 +1,9 @@
 import {heatmap} from './heatmap.js';
 import {edgeboundary} from './edgeboundary.js';
 import {pcoaPlot} from './pcoaPlot.js';
+import {bubblePlot} from './bubblePlot.js';
 
 export {heatmap};
 export {edgeboundary};
 export {pcoaPlot};
+export {bubblePlot};
