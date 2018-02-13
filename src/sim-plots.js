4c267344df9d5b22763a54a428a1846950dcf13b
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-02-13 13:00:30 -0700

    Fix exports

--- src/sim-plots.js
+++ src/sim-plots.js
@@ -1,9 +1,11 @@
 import {heatmap} from './heatmap.js';
+import {symmetricalHeatmap} from './heatmap.js';
 import {edgeboundary} from './edgeboundary.js';
 import {pcoaPlot} from './pcoaPlot.js';
 import {bubblePlot} from './bubblePlot.js';
 
 export {heatmap};
+export {symmetricalHeatmap};
 export {edgeboundary};
 export {pcoaPlot};
 export {bubblePlot};
