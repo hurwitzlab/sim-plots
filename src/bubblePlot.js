44bf366925fabd74f3b31464928fe1aca534ff05
Matt Bomhoff <mbomhoff@email.arizona.edu>
2018-09-13 15:48:04 -0700

    Update bubble plot for changes to Centrifuge app output file format

--- src/bubblePlot.js
+++ src/bubblePlot.js
@@ -9,40 +9,56 @@ function bubblePlot(id, data, params) { // TODO split data processing and render
     var scores = d3.csv.parse(data);
     var columns = {};
     var rows = {};
-    var maxValue = 10;
+    var maxValue = 0;
+
+//    console.log("data:", data);
+//    console.log("scores:", scores);
+
     scores.forEach(function(row) {
+        if (!row.sample || !row.name || !row.proportion)
+            console.error("sim-plots:bubblePlot: missing required field: ", row);
+
         columns[row.sample] = 1;
 
-        if (typeof rows[row.Name] === 'undefined')
-            rows[row.Name] = {};
-        rows[row.Name][row.sample] = row.Abundance;
+        if (typeof rows[row.name] === 'undefined')
+            rows[row.name] = {};
+        rows[row.name][row.sample] = row.proportion * 1; // convert to int with *1
 
-        maxValue = Math.max(maxValue, row.Abundance);
+        maxValue = Math.max(maxValue, row.proportion);
     });
+
+//    console.log("maxValue:", maxValue);
+//    console.log("columns:", columns);
+//    console.log("rows:", rows);
 
     var rows2 = [];
     Object.keys(rows).sort().reverse().forEach(function(name) {
         var values =
             Object.keys(columns).map(sample => {
-                return [ (rows[name][sample] || 0) / maxValue, 0.7 ];
+                return [ (rows[name][sample] || 0) / maxValue, 0 ]; // don't know what the second value is for
             });
+
         rows2.push({
             name: name,
             values: values
         });
     });
 
-    var data2 = { columns: Object.keys(columns), rows: rows2 };
+    var data2 = {
+        columns: Object.keys(columns),
+        rows: rows2
+    };
+//    console.log("data2:", data2);
 
     var width = Object.keys(columns).length * 200;
     var height = Object.keys(rows).length * 40;
 
-    var chart = d3.select('#'+id).append('svg')
-                  .chart('BubbleMatrix')
-                  .width(width).height(height)
-                  .slanted(true);
-
-    chart.draw(data2);
+    d3.select('#'+id)
+    .append('svg')
+    .chart('BubbleMatrix')
+    .width(width).height(height)
+    .slanted(true)
+    .draw(data2);
 }
 
 export { bubblePlot };
